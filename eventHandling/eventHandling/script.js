36f7f219a5c065cdb893f8c064648fc4aa44ba29
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-11-14 10:49:32 -0800

    Switch to DOMContentLoaded

--- eventHandling/eventHandling/script.js
+++ eventHandling/eventHandling/script.js
@@ -1,12 +1,18 @@
-window.onload = function () {
-  var btn = document.querySelector('button');
+// window.onload = function () {
 
-  function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-  }
+// };
 
-  btn.onclick = function () {
-    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
-  };
-};
+ document.addEventListener('DOMContentLoaded', () => {
+
+   var btn = document.querySelector('button');
+   btn.addEventListener('click', function () {
+     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+     document.body.style.backgroundColor = rndCol;
+   })
+ } )
+
+
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
