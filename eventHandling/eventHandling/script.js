aa51a79af196f4371e6d75d24240d423dca39851
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-11-16 07:55:45 -0800

    Bind bgchange to buttons and keydown

--- eventHandling/eventHandling/script.js
+++ eventHandling/eventHandling/script.js
@@ -2,17 +2,34 @@
 
 // };
 
- document.addEventListener('DOMContentLoaded', () => {
+//  document.addEventListener('DOMContentLoaded', () => {
 
-   var btn = document.querySelector('button');
-   btn.addEventListener('click', function () {
+//    var btn = document.querySelector('button');
+//    btn.addEventListener('click', function () {
+//      var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+//      document.body.style.backgroundColor = rndCol;
+//    })
+//  } )
+   
+
+
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+
+ document.addEventListener('DOMContentLoaded', () => {
+   var buttons = document.querySelectorAll('button');
+   function random(number) {
+     return Math.floor(Math.random() * number);
+   }
+   function bgChange() {
      var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
      document.body.style.backgroundColor = rndCol;
-   })
- } )
+   }
+   for (let i = 0; i < buttons.length; i++) {
+     buttons[i].onmouseover = bgChange;
+   }
 
+   window.onkeydown = bgChange;
+ }); 
 
-
-function random(number) {
-  return Math.floor(Math.random() * (number + 1));
-}
