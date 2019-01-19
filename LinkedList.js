3d5bbe4da6f20f71e701a247548c5d1b5d26e60a
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-01-18 16:22:29 -0800

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -4,31 +4,31 @@ function Node(element) {
 }
 
 function LinkedList() {
-  let length = 0;
-  let head = null;
+  this.length = 0;
+  this.head = null;
 
 
   this.add = function(element) {
     const newNode = new Node(element)
-    if(head === null) {
-      head = newNode
+    if(this.head === null) {
+      this.head = newNode
     } else {
-      let currentNode = head
+      let currentNode = this.head
       while(currentNode.next) {
         currentNode = currentNode.next
       }
       currentNode.next = newNode
     }
     
-    length++;
+    this.length++;
   }
 
-  this.length = function() {
-    return length
+  this.showLength = function() {
+    return this.length
   }
 
   this.show = function() {
-    let currentNode = head;
+    let currentNode = this.head;
     let i = 1;
     while(currentNode) {
       console.log(`Current Node Number ${i}`, currentNode.element);
@@ -36,16 +36,46 @@ function LinkedList() {
       i++
     }
   }
-  //remove
+
+  this.removeHead = function() {
+    if (this.length === 0) return undefined
+    const previousHeadValue = this.head.element
+    this.head = this.head.next
+    this.length--
+
+    return previousHeadValue
+  }
+
+  this.remove = function(element) {
+    if (this.length === 0) return undefined 
+    if (this.head.element === element) this.removeHead()
+
+    let previousNode = this.head
+    let currentNode = previousNode.next
+    
+    while (currentNode) {
+      if (currentNode.element === element) break;
+
+      previousNode = currentNode 
+      currentNode = currentNode.next
+    }
+
+    if (currentNode === null) return undefined
+
+    previousNode.next = currentNode.next
+    this.length--
+    return this
+  }
   
 }
 
 
 const list = new LinkedList()
+
+list.add("a")
+list.add("ab")
 list.add("abc")
-list.add("asdfasdf")
-list.add("asdfasdfasdf")
-
-
-console.log(list.length())
-list.show();
+list.add("abcd")
+list.show()
+list.remove("ab")
+list.show()
