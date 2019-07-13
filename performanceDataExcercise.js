02ebaf1c21d4a325c4704a0a51bba8cecc6b6dac
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-07-12 21:14:50 -0700

    Create PerformanceChartData.js

---
+++ performanceDataExcercise.js
@@ -0,0 +1,11 @@
+createPerformanceChartData(resp, interval) {
+		// [{"date":"2017-01-01","value":100},{"date":"2017-01-01","value":100}]
+		// Create a new array 
+		// declare count 
+		// Iterate through the resp 
+		for (let i in resp) {
+			
+	 	}
+	 
+
+	}
