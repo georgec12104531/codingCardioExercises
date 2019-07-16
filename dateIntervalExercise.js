49cbb7a557df0a7337aee1af7fcb850040ae4952
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-07-16 00:18:21 -0700

    Create dateIntervalExercise.js

---
+++ dateIntervalExercise.js
@@ -0,0 +1,627 @@
+// let resp = [
+//   {
+//     "date":"2017-01-01",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-02",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-03",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-04",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-05",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-06",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-07",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-08",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-09",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-10",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-11",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-12",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-13",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-14",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   },
+//   {
+//     "date": "2017-01-10",
+//     "io_count": 100,
+//     "proposal_count": 100,
+//     "io_value": 100
+//   }
+// ]
+
+let resp = 
+[ 
+  { date: '06-14-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-15-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-16-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-17-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-18-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-19-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-20-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-21-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-22-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-23-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-24-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-25-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-26-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-27-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-28-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-29-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '06-30-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-01-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-02-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-03-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-04-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-05-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-06-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-07-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-08-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-09-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-10-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-11-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-12-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-13-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-14-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-15-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-16-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-17-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-18-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-19-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-20-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-21-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-22-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-23-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-24-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-25-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-26-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-27-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-28-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-29-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-30-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '07-31-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-01-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-02-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-03-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-04-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-05-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-06-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-07-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-08-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-09-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-10-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-11-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-12-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-13-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-14-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-15-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-16-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-17-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-18-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-19-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-20-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-21-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-22-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-23-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-24-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-25-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-26-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-27-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-28-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-29-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-30-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '08-31-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-01-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-02-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-03-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-04-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-05-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-06-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-07-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-08-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-09-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-10-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-11-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-12-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-13-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-14-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-15-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-16-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-17-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-18-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-19-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-20-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 },
+  { date: '09-21-2019',
+    io_count: 100,
+    proposal_count: 100,
+    io_value: 100 } 
+]
+
+function calculateDateDifference(dateOne, dateTwo) {
+  let startDate = moment(dateOne, "MM-DD-YYYY");
+  let endDate = moment(dateTwo, "MM-DD-YYYY");
+  return moment.duration(endDate.diff(startDate)).asDays() + 1;
+}
+
+function calculateInterval(dateDifferent) {
+  let interval;
+  if (dateDifference <= 12 ) {
+    interval = 1;
+  } else if (dateDifference <= 84) {
+    interval = 7;
+  } else if (dateDifference <= 365) {
+    interval = 30;
+  };
+}
+
+function reset() {
+  const newObj = {
+    'date': '',
+    'io_count': 0,
+    'proposal_count': 0,
+    'io_value': 0
+  }
+
+  return newObj;
+}
+
+function isMonthStart(dateObj) {
+  console.log('dateObj inside', dateObj)
+  if (dateObj === undefined) return;
+  let dateParts = dateObj["date"].split("-");
+
+  console.log(dateParts)
+  let day = dateParts[1];
+  console.log(day)
+  console.log(day === "01")
+  return day === "01";
+}
+
+
+function createPerformanceChartData(resp, interval) {
+  let result = [];
+  let count = 0;
+  let tempObj = reset();
+
+  for (let i in resp) {
+    current = resp[i];
+    console.log('cicurrent)
+    if (count < interval) {
+      if (count === 0) {
+        tempObj['date'] = current['date'];
+      }
+      // Proposal Count
+      tempObj['proposal_count'] += parseInt(current['proposal_count']);
+      // IO Count
+      tempObj['io_count'] += parseInt(current['io_count']);
+      // IO Value
+      tempObj['io_value'] += parseInt(current['io_value']);
+      count += 1
+    } else {
+      // Reset obj when interval is reached
+      result.push(tempObj)
+      count = 0;
+      tempObj = reset();
+      if (count === 0) {
+        tempObj['date'] = current['date'];
+      }
+      // Proposal Count
+      tempObj['proposal_count'] += parseInt(current['proposal_count']);
+      // IO Count
+      tempObj['io_count'] += parseInt(current['io_count']);
+      // IO Value
+      tempObj['io_value'] += parseInt(current['io_value']);
+      count += 1
+    }
+  }   
+  result.push(tempObj);
+  return result;
+}
+
+function formatPerformanceChartMonthlyData(resp) {
+  let result = [];
+  let tempObj = reset();
+  let current;
+  let next;
+  
+  for (let i = 0; i < resp.length; i++) {
+    current = resp[i];
+    console.log('i', i)
+    console.log('current', current)
+    next = resp[i + 1];
+    console.log('next', next)
+    // Date is not first day of the month
+    if (isMonthStart(current)) {
+      console.log('YO', current)
+      tempObj = reset();
+      tempObj['date'] = current['date'];
+      // Proposal Count
+      tempObj['proposal_count'] += parseInt(current['proposal_count']);
+      // IO Count
+      tempObj['io_count'] += parseInt(current['io_count']);
+      // IO Value
+      tempObj['io_value'] += parseInt(current['io_value']);
+    // If next date is month start
+    } else if (isMonthStart(next)) {
+      console.log('SUP!!!!')
+      // Proposal Count
+      tempObj['proposal_count'] += parseInt(current['proposal_count']);
+      // IO Count
+      tempObj['io_count'] += parseInt(current['io_count']);
+      // IO Value
+      tempObj['io_value'] += parseInt(current['io_value']);
+      result.push(tempObj);
+    } else {
+      console.log('ELSEEEEEEEE')
+      // Proposal Count
+      tempObj['proposal_count'] += parseInt(current['proposal_count']);
+      // IO Count
+      tempObj['io_count'] += parseInt(current['io_count']);
+      // IO Value
+      tempObj['io_value'] += parseInt(current['io_value']);
+    }
+  }
+  result.push(tempObj);
+  return result;
+}
+
+
+console.log(formatPerformanceChartMonthlyData(resp))
