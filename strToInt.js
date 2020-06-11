ef78c60e651e8d6bdbc596eb494a15c5fe809ae1
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-06-11 12:58:22 -0700

    Create strToInt.js

---
+++ strToInt.js
@@ -0,0 +1,12 @@
+// Converting strings into integers without built in functions
+
+// The ASCII character ‘0’ is represented by 48 (decimal)
+
+// The ASCII character ‘9’ is represented by 57 (decimal)
+
+// The ASCII characters ‘1’–’8′ are between them…
+
+// So, '7' - '0' is exactly the same operation as 55 - 48, so you get a 7…
+
+const newNum = '2' - '0';
+console.log(typeof newNum)
